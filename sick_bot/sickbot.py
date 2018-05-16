14be0f471e6b219cf9da7152cf1bcfa532ab99da
Reckhouse <37946446+Reckhouse@users.noreply.github.com>
2018-05-16 15:31:50 -0600

    Add files via upload

---
+++ sick_bot/sickbot.py
@@ -0,0 +1,26 @@
+import discord
+import asyncio
+import requests
+import json
+
+client = discord.Client()
+user = 'Vh1Xu47ThdGBdHZi'
+key = '5BSi6mgVNV5cKzMV3uQHHz5fPZ0z40S2'
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
+    await client.change_presence(game=discord.Game(name='SickBot V0.2'))
+
+@client.event
+async def on_message(message):
+    if not message.author.bot and (message.server == None or client.user in message.mentions):
+        await client.send_typing(message.channel)
+        txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
+        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'sickbot', 'text':txt}).text)
+        if r['status'] == 'success':
+            await client.send_message(message.channel, message.author.mention+' '+r['response'])
+
+print('Starting...')
+requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'sickbot'})
+client.run('NDMyNjMwMjMzNjUzNDQ0NjE4.Daw34g.rEBW--cS61N-o4XDNITdlIjwoTI')
